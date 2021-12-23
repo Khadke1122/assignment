27933af984e3bed4eb630068802441448c97d215
Khadke1122 <95537360+Khadke1122@users.noreply.github.com>
2021-12-23 18:53:53 +0530

    Add files via upload

---
+++ assignments/assignment 7/simple calculator/1.js
@@ -0,0 +1,22 @@
+function calculate()
+{
+            var x = Number(document.getElementById('value1').value);
+            var y =Number(document.getElementById('value2').value)  
+            var z =document.getElementById('sign').value;
+
+            if(z=="-") {
+                document.getElementById('result').value=x-y;
+            }
+            else if(z=="+"){
+                document.getElementById('result').value=x+y;
+           } 
+           else if (z=="*"){
+               document.getElementById('result').value=x*y;
+           }
+           else if(z=="/"){
+               document.getElementById('result').value=x/y;
+           }
+           else{
+               window.alert("please enter option")
+           }
+}
